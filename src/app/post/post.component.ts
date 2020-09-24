6171d4312d390674aad3707a4ebdcadcd2e7e491
Shkurdov <anton.shkurdov@vrpconsulting.com>
2020-09-24 12:02:59 +0300

    3-7

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ContentChild, ElementRef, Input, OnInit } from '@angular/core';
 import { Post } from '../app.component';
 
 @Component({
@@ -9,10 +9,12 @@ import { Post } from '../app.component';
 export class PostComponent implements OnInit {
 
   @Input() post:Post // так мы через декоратор инпортируем внешние данные в компонент (в данном случае посты)
+  @ContentChild('info', {static: true}) infoRef: ElementRef
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.infoRef.nativeElement)
   }
 
 }
