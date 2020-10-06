4b7beaaa74d314f3822fcfcb4b3e663269272275
Shkurdov <anton.shkurdov@vrpconsulting.com>
2020-10-06 17:44:36 +0300

    7-3 updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -11,10 +11,9 @@ import {FormsModule} from '@angular/forms'
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [
-
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
