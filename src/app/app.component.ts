999f5ae68b09c4c896f4b6146d07144a851b30c1
Shkurdov <anton.shkurdov@vrpconsulting.com>
2020-10-07 16:46:27 +0300

    8-4 updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,58 @@
 import {Component, OnInit} from '@angular/core'
-import {FormGroup} from '@angular/forms'
+import {HttpClient} from '@angular/common/http'
+import {delay} from 'rxjs/operators';
+
+export interface Todo {
+  completed: boolean
+  title: string
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
-  form: FormGroup
+
+  todos: Todo[] = []
+  loading = false
+  todoTitle = ''
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.form = new FormGroup({})
+    this.fetchTodos()
   }
 
-  submit() {
-    console.log('Form submitted: ', this.form)
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return
+    }
+
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false
+    }
+
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(todo => {
+        this.todos.push(todo)
+        this.todoTitle = ''
+      })
+
   }
+
+  fetchTodos() {
+    this.loading = true
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+    .pipe(delay(1500))
+    .subscribe(todos => {
+      this.todos = todos
+      this.loading = false
+    })
+  }
+
 }
 
