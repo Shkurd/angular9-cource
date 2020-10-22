ccc143d7edb322ae9296cc48441df5cf6545f699
Shkurdov <anton.shkurdov@vrpconsulting.com>
2020-10-22 17:27:48 +0300

    9-11

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,25 +1,25 @@
 import {Component, OnInit} from '@angular/core'
-import { ActivatedRoute, Params, Router} from '@angular/router';
-import { Post, PostsService } from '../posts.service';
+import {ActivatedRoute, Params, Router} from '@angular/router'
+import {Post, PostsService} from '../posts.service'
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit{
+export class PostComponent implements OnInit {
 
   post: Post
 
-  constructor (
+  constructor(
     private route: ActivatedRoute,
-    private router: Router, 
-    private postService: PostsService) {}
+    private router: Router,
+    private postsService: PostsService
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe ((params: Params) => {
-      console.log('params', params)
-      this.post = this.postService.getById(+params.id)
+    this.route.params.subscribe((params: Params) => {
+      this.post = this.postsService.getById(+params.id)
     })
   }
 
