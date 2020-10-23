e4633615e60ec50ddeb5120e32a20907526ce80d
Shkurdov <anton.shkurdov@vrpconsulting.com>
2020-10-23 16:12:52 +0300

    block routing finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {PostsComponent} from './posts/posts.component'
 import {PostComponent} from './post/post.component'
 import { AboutExtraComponent } from './about-extra/about-extra.component'
 import { ErrorPageComponent } from './error-page/error-page.component'
+import { AuthGuard } from './auth.guard'
 
 // http://localhost:4200/ -> HomeComponent
 // http://localhost:4200/about -> AboutComponent
@@ -13,10 +14,10 @@ import { ErrorPageComponent } from './error-page/error-page.component'
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent, children: [
+  {path: 'about', component: AboutComponent, canActivateChild: [AuthGuard], children: [
     {path: 'extra', component: AboutExtraComponent}
   ] },
-  {path: 'posts', component: PostsComponent},
+  {path: 'posts', component: PostsComponent, canActivate: [AuthGuard]},
   {path: 'posts/:id', component: PostComponent},
   {path: 'error', component: ErrorPageComponent},
   {path: '**', redirectTo: '/error'}
