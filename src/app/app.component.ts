e8da0151d18c1965ad7ca638171b9ce6619d80e0
Shkurdov <anton.shkurdov@vrpconsulting.com>
2020-10-30 15:00:24 +0300

    SEO and metatags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core'
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core'
+import { Meta, Title } from '@angular/platform-browser'
+import { ModalComponent } from './modal/modal.component'
+import { RefDirective } from './modal/ref.directive'
 
 @Component({
   selector: 'app-root',
@@ -6,6 +9,35 @@ import {Component} from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  modal = false
+
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private title: Title,
+    private meta: Meta,
+
+  ) {
+    const t = title.getTitle()
+    title.setTitle('new title')
+    const m = meta.addTags([
+      { name: 'keywords', content: 'Angular keywords, key, key, key' },
+      { name: 'description', content: 'Angular description!' }
+    ])
+
+  }
+
+  @ViewChild(RefDirective, {static: false}) RefDir: RefDirective
+
+  showModal() {
+    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent)
+    this.RefDir.containerRef.clear()
+
+    const component = this.RefDir.containerRef.createComponent(modalFactory)
+    component.instance.title = 'Dynamic title'
+    component.instance.close.subscribe(
+      () => {
+        this.RefDir.containerRef.clear()
+      }
+    )
+  }
 }
 
