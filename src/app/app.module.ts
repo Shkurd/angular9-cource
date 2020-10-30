f3fdb702d9eea1daeb1c4343e1aa3455471ffd9f
Shkurdov <anton.shkurdov@vrpconsulting.com>
2020-10-30 12:43:46 +0300

    dynamic componebt import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,17 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms'
-import {HomePageComponent} from './home-page/home-page.component'
-import {AppRoutingModule} from './app-routing.module'
-import {AboutPageComponent} from './about-page/about-page.component'
-import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
-import {ColorDirective} from './shared/color.directive'
-import {PageNamePipe} from './shared/page-name.pipe'
+import {FormsModule} from '@angular/forms';
+import { ModalComponent } from './modal/modal.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    AboutPageComponent,
-    AboutExtraPageComponent,
-    ColorDirective,
-    PageNamePipe
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
